8c1a737c93d1fbecafc471c17d4dc4177b9e55de
Franek <michalakfranciszek2005@gmail.com>
2025-11-09 19:34:22 +0100

    Added example program and compilation scripts

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
 #include "src/ansiPrint.hpp"
 
 int main(){
-    ANSI::mode params[3]{
-        ANSI::mode::blink,
-        ANSI::mode::bold,
-        ANSI::mode::underline
-    };
-    int size = 3;
-    //ANSI::println("test",params,size);
-    // ANSI::printColorln("test", ANSI::Color::blue);
-    ANSI::printMultiln("test", params, 3);
-    ANSI::printErr("DUPA");
     return 0;
 }
