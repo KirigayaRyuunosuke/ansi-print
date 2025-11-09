0db9b52251975b6179b0d3c7adb2fc8cf7e93d59
Franek <michalakfranciszek2005@gmail.com>
2025-11-09 19:53:02 +0100

    File structure updated

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++ main/main.cpp src/*.cpp -o program;  # compile
-./program;                          # run
-rm -f program;                      # remove
+g++ main/main.cpp src/*.cpp -o program;     # compile
+./program;                                  # run
+rm -f program;                              # remove
